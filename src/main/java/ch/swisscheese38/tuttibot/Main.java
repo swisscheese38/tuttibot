526f55cff20a008538588ce979f4f35cbe24fb2a
swisscheese38 <roland.wehrlin@gmail.com>
2024-08-24 23:08:57 +0200

    Traverse item divs

--- src/main/java/ch/swisscheese38/tuttibot/Main.java
+++ src/main/java/ch/swisscheese38/tuttibot/Main.java
@@ -1,9 +1,11 @@
 package ch.swisscheese38.tuttibot;
 
 import java.io.IOException;
+import java.util.List;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import org.jsoup.select.Elements;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -14,6 +16,10 @@ public class Main {
     public static void main(String... args) throws IOException {
         final String url = "https://www.tutti.ch/de/q/suche/?sorting=newest&page=1&query=puch+maxi";
         final Document doc = Jsoup.connect(url).get();
-        logger.info(doc.title());
+
+        final Elements itemDivs = doc.select("div[data-private-srp-listing-item-id]");
+        final List<String> itemIds = itemDivs.eachAttr("data-private-srp-listing-item-id");
+
+        itemIds.forEach(logger::info);
     }
 }
